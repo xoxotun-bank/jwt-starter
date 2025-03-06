acaed3dde331b2cb0fd036ee81649143e73f9637
oxid322 <rust0267@gmail.com>
2025-03-07 01:29:41 +1000

    init

---
+++ src/main/java/com/example/jwtstarter/JwtStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtStarterApplication.class, args);
+    }
+
+}
